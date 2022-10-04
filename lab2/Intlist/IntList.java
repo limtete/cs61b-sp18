44feaec03485e025d966595b255c312bb9786d19
limtete <come.across.time@gmail.com>
2022-10-04 17:40:43 +0800

    Lab2: debug.

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,8 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        if (A == null || B == null) return null;
+        if (A == null) return B;
+        if (B == null) return null;
         IntList L = A;
         while (L.rest != null) L = L.rest;
         L.rest = B;
@@ -95,7 +96,8 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        if (A == null || B == null) {
+        if (A == null) {return B;}
+        if (B == null) {
             return null;
         };
 
